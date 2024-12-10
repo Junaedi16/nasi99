7da3f3cbd484edb0d7e9cfb66d73c184ce358e0c
Junaedi16 <105527843+Junaedi16@users.noreply.github.com>
2024-12-10 23:18:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const orderButton = document.getElementById("orderButton");
+  orderButton.addEventListener("click", () => {
+    window.location.href = "menu.html";
+  });
+});
